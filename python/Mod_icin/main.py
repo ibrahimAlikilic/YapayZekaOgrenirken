f5fb71adbc9a8b61c1bfd4bca3e29d1c666c2214
ibrahim-alikilic <ibrahim.alikilic@gmail.com>
2023-11-27 21:43:48 +0300

    Commit olmama sebebi
    Sonradan yüklüyor olmam

---
+++ python/Mod_icin/main.py
@@ -0,0 +1,6 @@
+import mod
+result=help(mod)
+print(result)
+print(mod.number)
+p=mod.Person()
+p.Speak()
---
+++ python/Mod_icin/mod.py
@@ -0,0 +1,17 @@
+print("modül eklendi")
+
+number=10
+
+numbers=[1,2,3]
+
+person={
+    "name":"ali",
+    "age":20,
+    "city":"Sakarya"
+}
+
+def func(x):
+    print(f"func çalıştırıldı ve x : {x}")
+class Person():
+    def Speak(self):
+        print("I am speaking ...")
