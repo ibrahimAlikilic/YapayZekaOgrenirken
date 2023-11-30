68a51e008c0d8ce57876b63adcec76d65f25350b
ibrahim-alikilic <ibrahim.alikilic@gmail.com>
2023-11-30 19:32:26 +0300

    Not ekledim

--- pyimagesearch/step4NuildMiniProjeWithOpenCv/src/detect_color.py
+++ pyimagesearch/step4NuildMiniProjeWithOpenCv/src/detect_color.py
@@ -15,6 +15,19 @@ boundaries = [
 	([25, 146, 190], [62, 174, 250]),
 	([103, 86, 65], [145, 133, 128])
 ]
+'''
+Bu kod bir renk aralığını belirleyip bu aralıktaki renkleri bulan ve görselleştiren bir örnek içerir. boundaries adlı liste, renk aralıklarını belirten dört farklı renk aralığını içerir.
+
+Her bir renk aralığı iki adet NumPy dizisi içerir: alt sınır ve üst sınır. Her bir renk sınırı, BGR (Mavi, Yeşil, Kırmızı) renk uzayında belirtilmiştir. Yani, her renk aralığı, 
+bu renklerin belirli bir aralığını tanımlar.
+
+Örneğin, [17, 15, 100] ve [50, 56, 200] renk aralığı, mavi renk tonları için bir aralığı temsil eder. Bu renk aralığı, alt sınırdaki mavi rengin (17, 15, 100) ve 
+üst sınırdaki mavi rengin (50, 56, 200) olması anlamına gelir.
+
+Her bir renk aralığı, cv2.inRange fonksiyonunda kullanılmak üzere uygun NumPy dizilere dönüştürülür. cv2.inRange fonksiyonu, 
+belirtilen renk aralığına giren pikselleri beyaz, diğer pikselleri siyah yapacak bir maske oluşturur. Bu maske, ardından cv2.bitwise_and fonksiyonu ile 
+orijinal görüntü üzerine uygulanır ve yalnızca belirtilen renk aralığındaki pikseller gösterilir.
+'''
 # loop over the boundaries
 for (lower, upper) in boundaries:
 	# create NumPy arrays from the boundaries
