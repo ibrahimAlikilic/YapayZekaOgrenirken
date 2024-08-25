b129b13963d1f43ebdef13fb9313759564e78eed
ibrahimAlikilic <ibrahim.alikilic@gmail.com>
2024-08-25 13:08:10 +0300

    Dosya yolu düzenlendi

--- DerinOgrenmeileGoruntuIsleme/EvrisimliSinirAglariIleNesneTespiti/YOLO/input/yolo_model.py
+++ DerinOgrenmeileGoruntuIsleme/EvrisimliSinirAglariIleNesneTespiti/YOLO/input/yolo_model.py
@@ -27,7 +27,7 @@ class YOLO:
         """
         self._t1 = obj_threshold
         self._t2 = nms_threshold
-        self._yolo = load_model("../input/data/data/yolo.h5")
+        self._yolo = load_model("../../../../../Masaüstü/YOLOlar/yolo.h5")
 
     def _process_feats(self, out, anchors, mask):
         """Process output features.
--- DerinOgrenmeileGoruntuIsleme/EvrisimliSinirAglariIleNesneTespiti/YOLO/src/yolo3.py
+++ DerinOgrenmeileGoruntuIsleme/EvrisimliSinirAglariIleNesneTespiti/YOLO/src/yolo3.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../../Masaüstü/YOLOlar')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../input')))
 from yolo_model import YOLO 
 
 #########################################################
