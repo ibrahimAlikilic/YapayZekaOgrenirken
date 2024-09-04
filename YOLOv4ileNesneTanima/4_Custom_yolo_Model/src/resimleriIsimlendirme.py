0fea23a1e3f134e34625b546f2383e0c9dabd00d
ibrahimAlikilic <ibrahim.alikilic@gmail.com>
2024-09-04 16:24:18 +0300

    Googleden resimler kısımnda çıkan sonuçları indiridiğim kod

---
+++ YOLOv4ileNesneTanima/4_Custom_yolo_Model/src/resimleriIsimlendirme.py
@@ -0,0 +1,18 @@
+import os
+
+# Fotoğrafların bulunduğu dizinin yolu
+directory = "../images"
+
+# Dizin içindeki dosyaları al ve yalnızca .jpg uzantılı olanları filtrele
+files = [f for f in os.listdir(directory)]
+
+# Dosyaları sıralı şekilde yeniden adlandır
+for i, filename in enumerate(sorted(files), 1):
+    old_file_path = os.path.join(directory, filename)
+    new_file_name = f"{i}.jpg"
+    new_file_path = os.path.join(directory, new_file_name)
+    
+    # Dosyayı yeniden adlandır
+    os.rename(old_file_path, new_file_path)
+
+print("Dosyalar başarıyla yeniden adlandırıldı!")
---
+++ python/Kolayliklar/GoogleResimler_indirme.py
@@ -0,0 +1,51 @@
+'''
+1) kod çalışırken gelen pencereyi hep aşağı indireceksin ki tüm resimler açılmış olsun
+2) window da chrome kullanman lazım diğer türlü olmaz.
+'''
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import os
+import requests
+
+# Google Görseller URL'sini oluştur
+query = "boston dynamics spot dog pictures"  # Buraya arama yapmak istediğiniz kelimeyi girin
+url = f"https://www.google.com/search?q={query}&source=lnms&tbm=isch"
+
+# Webdriver'ı başlat
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get(url)
+
+# Sayfanın yüklenmesini bekleyin
+time.sleep(2)
+
+# Tüm resim öğelerini bulun
+images = driver.find_elements(By.CSS_SELECTOR, "img")
+
+# Resimleri kaydedileceği klasörü oluşturun
+save_path = "images"
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+# Resimleri indir
+for idx, img in enumerate(images):
+    try:
+        img_url = img.get_attribute('src')
+        
+        # URL'in geçerli olup olmadığını kontrol edin
+        if img_url and img_url.startswith('http'):
+            img_data = requests.get(img_url).content
+            with open(f"{save_path}/{query}_{idx}Bir.jpg", 'wb') as handler:
+                handler.write(img_data)
+            print(f"{query}_{idx}.jpg indirildi.")
+        else:
+            print(f"Geçersiz URL tespit edildi: {img_url}")
+    
+    except Exception as e:
+        print(f"Bir hata oluştu: {e}")
+
+# Webdriver'ı kapat
+driver.quit()
