0d2f20341b8f34d1a818e7181fc840c0c77e7f67
ibrahimAlikilic <ibrahim.alikilic@gmail.com>
2024-09-04 20:40:45 +0300

    tes t=%20, training=%80 olacak şekilde txt'ler oluşturuldu

---
+++ YOLOv4ileNesneTanima/4_Custom_yolo_Model/src/training_test_Olustur.py
@@ -0,0 +1,12 @@
+# training = %80
+# test = %20
+i=1
+print("training")
+while i<186: # training
+    print(f"../../../../Masaüstü/DateSets/boston dynamics spot/{i}.jpg")
+    i+=1
+print("******************************************************************")
+print("test")
+while i<234: # test
+    print(f"../../../../Masaüstü/DateSets/boston dynamics spot/{i}.jpg")
+    i+=1 
